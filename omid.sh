448e17ce58f27d5c30322c822b8a4664a88becea
MinoVpnCh <somid4758@gmail.com>
2024-12-18 15:20:35 +0330

    Update omid.sh

--- omid.sh
+++ omid.sh
@@ -1,22 +1,34 @@
-#!/bin/bash -e
-echo
-echo "=== azadrah.org ==="
-echo "=== https://github.com/azadrahorg ==="
-echo "=== MikroTik 7 Installer ==="
-echo
-sleep 3
-wget https://download.mikrotik.com/routeros/7.16.1/chr-7.16.1.img.zip -O chr.img.zip  && \
-gunzip -c chr.img.zip > chr.img  && \
-STORAGE=`lsblk | grep disk | cut -d ' ' -f 1 | head -n 1` && \
-echo STORAGE is $STORAGE && \
-ETH=`ip route show default | sed -n 's/.* dev \([^\ ]*\) .*/\1/p'` && \
-echo ETH is $ETH && \
-ADDRESS=`ip addr show $ETH | grep global | cut -d' ' -f 6 | head -n 1` && \
-echo ADDRESS is $ADDRESS && \
-GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
-echo GATEWAY is $GATEWAY && \
-sleep 5 && \
-dd if=chr.img of=/dev/$STORAGE bs=4M oflag=sync && \
-echo "Ok, reboot" && \
-echo 1 > /proc/sys/kernel/sysrq && \
-echo b > /proc/sysrq-trigger && \
+#!/usr/bin/env bash
+#Auto install Mikrotik Chr
+#by asghar61
+# Must be root !
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+echo "Preparation ..."
+apt install unzip -y
+
+# Latest Stable
+CHR_VERSION=7.15.3
+
+# Environment
+DISK=$(lsblk | grep "disk" | head -n 1 | cut -d' ' -f1)
+INTERFACE=$(ip -o -4 route show to default | awk '{print $5}')
+INTERFACE_IP=$(ip addr show $INTERFACE | grep global | cut -d' ' -f 6 | head -n 1)
+INTERFACE_GATEWAY=$(ip route show | grep default | awk '{print $3}')
+
+wget -qO routeros.zip https://download.mikrotik.com/routeros/$CHR_VERSION/chr-$CHR_VERSION.img.zip && \
+unzip routeros.zip && \
+rm -rf routeros.zip
+
+mount -o loop,offset=512 chr-$CHR_VERSION.img /mnt
+
+echo "/ip address add address=${INTERFACE_IP} interface=[/interface ethernet find where name=ether1]
+/ip route add gateway=${INTERFACE_GATEWAY}
+" > /mnt/rw/autorun.scr
+
+umount /mnt
+echo u > /proc/sysrq-trigger
+dd if=chr-$CHR_VERSION.img of=/dev/${DISK} bs=100M
