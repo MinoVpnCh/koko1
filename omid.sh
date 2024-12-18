0ecacd1de6debe1bbe1cd118bb0e28999187b611
MinoVpnCh <somid4758@gmail.com>
2024-12-18 15:24:07 +0330

    Update omid.sh

--- omid.sh
+++ omid.sh
@@ -1,34 +1,12 @@
-#!/usr/bin/env bash
-#Auto install Mikrotik Chr
-#by asghar61
-# Must be root !
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
-
-echo "Preparation ..."
-apt install unzip -y
-
-# Latest Stable
-CHR_VERSION=7.15.3
-
-# Environment
-DISK=$(lsblk | grep "disk" | head -n 1 | cut -d' ' -f1)
-INTERFACE=$(ip -o -4 route show to default | awk '{print $5}')
-INTERFACE_IP=$(ip addr show $INTERFACE | grep global | cut -d' ' -f 6 | head -n 1)
-INTERFACE_GATEWAY=$(ip route show | grep default | awk '{print $3}')
-
-wget -qO routeros.zip https://download.mikrotik.com/routeros/$CHR_VERSION/chr-$CHR_VERSION.img.zip && \
-unzip routeros.zip && \
-rm -rf routeros.zip
-
-mount -o loop,offset=512 chr-$CHR_VERSION.img /mnt
-
-echo "/ip address add address=${INTERFACE_IP} interface=[/interface ethernet find where name=ether1]
-/ip route add gateway=${INTERFACE_GATEWAY}
-" > /mnt/rw/autorun.scr
-
-umount /mnt
+#!/bin/bash
+wget https://download.mikrotik.com/routeros/7.9/chr-7.9.img.zip -O chr.img.zip
+gunzip -c chr.img.zip > chr.img
 echo u > /proc/sysrq-trigger
-dd if=chr-$CHR_VERSION.img of=/dev/${DISK} bs=100M
+dd if=chr.img bs=1024 of=/dev/sda
+echo "sync disk"
+echo s > /proc/sysrq-trigger
+echo "Sleep 5 seconds"
+sleep 5
+echo "OK HAMVPN"
+echo "Ok, reboot"
+echo b > /proc/sysrq-trigger
